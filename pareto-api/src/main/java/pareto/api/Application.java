fe89f3586e963f7530fbb50e05b7a0b1b40d9816
Lucas Aquiles <lucasaquiles@gmail.com>
2019-05-08 11:56:10 -0300

    micronaut template app

---
+++ pareto-api/src/main/java/pareto/api/Application.java
@@ -0,0 +1,10 @@
+package pareto.api;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
